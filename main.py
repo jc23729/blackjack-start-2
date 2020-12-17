8b2617c995e980407ecd08c13f0e21ce6930fb0c
Juan Cruz <jc23729@gmail.com>
2020-12-17 23:37:56 +0000

    hints 1-7

--- main.py
+++ main.py
@@ -31,19 +31,54 @@
 #Hint 3: Download and read this flow chart I've created: 
 #   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
 
+
+
+
+
+
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
 #cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+
+def deal_card():
+  """Returns a random card from the deck"""
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+
+
 
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 #user_cards = []
 #computer_cards = []
+user_card = []
+computer_cards = []
+## first attempt, but rewrote in code below to make it less redundant
+# for _ in range(2):
+#   new_card = deal_card()
+#   user_cards.append(new_card)
+
+for _ in range(2):
+  user_cards.append(deal_card())
+  computer_cards.apppend(deal_card())
+
 
 #Hint 6: Create a function called calculate_score() that takes a List of cards as input 
 #and returns the score. 
 #Look up the sum() function to help you do this.
 
+# def calculate_scores(cards):
+#   return sum(cards)
+
 #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+def calculate_scores(cards):
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
+    
+  return sum(cards)
+
+
 
 #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
 
